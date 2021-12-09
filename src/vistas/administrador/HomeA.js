2e8ba34cec1011e34677d08cbee71aa3583a70a6
wmena-s <84716836+wmena-s@users.noreply.github.com>
2021-12-09 09:27:56 -0500

    merge

--- src/vistas/administrador/HomeA.js
+++ src/vistas/administrador/HomeA.js
@@ -1,11 +1,23 @@
 import HomeUE from "../home/HomeUE"
+import HomeUI from "../home/HomeUI"
+import './homeA.css'
+import Dashboard from './Dashboard'
 
 const homeA = () => {
     return (
-        <div>
+        <div className="contenedorUA">
+            <div>
             <h2>usuario Externo</h2>
             <HomeUE></HomeUE>
+            </div>
+            <div>
             <h2>usuario Interno</h2>
+            <HomeUI></HomeUI>
+            </div>
+            <div>
+                <h2>usaurio administrador</h2>
+                <Dashboard></Dashboard>
+            </div>
         </div>
     )
 }
