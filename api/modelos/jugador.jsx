1a7f1dcb29ce59fb38bf12a147497574b519ef9e
wmena-s <84716836+wmena-s@users.noreply.github.com>
2021-12-09 22:13:23 -0500

    malo

--- api/modelos/jugador.jsx
+++ api/modelos/jugador.jsx
@@ -1,21 +1,25 @@
-const mongoose = require('../conexion_mongo');  
+const mongoose = require('../conexion_mongo');
 const jugador = mongoose.model('jugadores', {
     nombre: {
         type: String,
         required: true,
     },
     cedula: {
-        type: String,
+        type: Number,
         required: true,
+        min: [3, 'Too few eggs'],
+        unique: true,
+        max: 8,
     },
     edad: {
         type: String,
         required: true,
     },
-    equipo: {
-        type: String,
-        default: "",
-    },
+    equipo: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'CrearEquipo',
+        required: false
+    }],
     campeonato: {
         type: String,
         default: "",
--- src/vistas/usuario_externo/ListaJugadores.js
+++ src/vistas/usuario_externo/ListaJugadores.js
@@ -4,25 +4,10 @@ import Character from "./Character"
 import './ListaJugadores.css'
 import Chart from '../../ChartV'
 
-//import BarChart from '../../BarChart';
-            {/* poner cuadro de busqued */
-            /* quitar fondo azul */}
-            
-          
-
 
 const ListaJugadores = () => {
 
-   /* useEffect( async ()=>{
-        
-            const respuesta =  await fetch(`${Constantes.RUTA_API}`);
-            const uno = await  respuesta.json();
-            console.log(uno)
-        
-    })*/
     const [user, setUser] = useState([]);
-
-
 
     useEffect(()=>{
         fetch(`${Constantes.RUTA_API3}`)
@@ -31,58 +16,19 @@ const ListaJugadores = () => {
     },[]);
 
     return (
-        
         <>
         <div className="contenedor1 otro">
             <h1 className="titulo1">LISTA DE JUGADORES</h1>
-            <p className="pp">listados de jugadores inscritos en el equipo: ---------------xxxxxxxxxxx-------------------</p>
+            <p className="pp">listados de jugadores inscritos en el equipo:</p>
             <div className="contenedorv1">
+                
                 <Character caracteres={user}></Character>
-                      {/**  <div>
-                <div>
-                    <p>DATOS JUGADOR</p>
-                    <li>nombre: {user.name}</li>
-                    <li>correo: {user.correo}</li>
-                    <li>usuario: {user.usuario}</li>
-                    <li>id: {user.id}</li>
-                </div>
-                <img className="img-clase" src="https://i.pinimg.com/236x/9f/4e/c0/9f4ec0f8b45f868b7d77166177538610.jpg"></img>
-                <button class="btn btn-primary" class="btn btn-primary">añadir</button>
-                <button class="btn btn-primary" class="btn btn-danger">eliminar</button>
-
-            </div>
-            <div>
-                 <div>
-                    <p>DATOS JUGADOR</p>
-                    <li>DATO UNO</li>
-                    <li>DATO DOS</li>
-                    <li>DATO TRES</li>
-                    <li>DATO CUATRO</li>
-                </div>
-                <img className="img-clase"  src="https://www.blogdebasket.com/files/20.03/michael-jordan-libro.png"></img>
-                <button class="btn btn-primary" class="btn btn-primary">añadir</button>
-                <button class="btn btn-primary" class="btn btn-danger">eliminar</button>
-            </div>
-            <div>
-                 <div>
-                    <p>DATOS JUGADOR</p>
-                    <li>DATO UNO</li>
-                    <li>DATO DOS</li>
-                    <li>DATO TRES</li>
-                    <li>DATO CUATRO</li>
-                </div>
-                <img className="img-clase"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsE8-IM_OpAMzVQNeJ3xOCWhVIJrdFJ9FpqCIupjrNMuyXj3Vnu_lHWi84mWE98PpnUQU&usqp=CAU"></img>
-                <button class="btn btn-primary" class="btn btn-primary">añadir</button>
-                <button class="btn btn-primary" class="btn btn-danger">eliminar</button>
-            </div>
-             */}
+                     
             </div>
             <div className="canvass">
                 <Chart x={user.map((item)=>(item.nombre))} y={user.map((item)=>(item.edad))} titulo="edad" namex="nombre" namey="edad"/>
-   
             </div>
-         {/**X   <BarChart />*/}
-            
+  
         </div>
         </>
        )
